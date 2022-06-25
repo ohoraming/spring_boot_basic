e7ce80397274155229e31d274e8c69a5dede1683
ohoraming <kminsmile@gmail.com>
2022-06-25 23:00:15 +0900

    회원 domain, repository 생성 및 testcase 작성

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,23 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id; // system이 정하는 id
+    private String name; // user가 입력하는 name
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
